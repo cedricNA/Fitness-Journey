6a6d04d3c294a606d38c20b3c13714869072ead0
Cedric Na <122440748+cedricNA@users.noreply.github.com>
2025-06-07 20:21:01 +0200

    add FoodItem interface and type meals state

---
+++ project/types.ts
@@ -0,0 +1,12 @@
+export interface FoodItem {
+  name: string;
+  calories: number;
+  protein: number;
+  carbs: number;
+  fat: number;
+  category?: string;
+  id?: number;
+  type?: string;
+  time?: string;
+  quantity?: number;
+}
