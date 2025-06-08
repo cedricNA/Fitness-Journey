d285fc048fe4e012640a738e56ecc6a48096ce92
Cedric Na <122440748+cedricNA@users.noreply.github.com>
2025-06-08 11:01:49 +0200

    feat: add global theme toggle

--- project/app/_layout.tsx
+++ project/app/_layout.tsx
@@ -3,17 +3,20 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import { UserProvider } from '@/context/UserContext';
+import { ThemeProvider } from '@/context/ThemeContext';
 
 export default function RootLayout() {
   useFrameworkReady();
 
   return (
-    <UserProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </UserProvider>
+    <ThemeProvider>
+      <UserProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </UserProvider>
+    </ThemeProvider>
   );
 }
